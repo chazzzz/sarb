0cc8fefc3d20d37539c9115a78d374caf126f0a5
chastineb1 <chastine@ideyatech.com>
2015-05-22 15:48:58 +0800

    SARB-8, SARB-9: Created base project, added junit test for getting successful server response

--- src/main/java/org/sarb/web/controller/HomeController.java
+++ src/main/java/org/sarb/web/controller/HomeController.java
@@ -3,15 +3,17 @@ package org.sarb.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by chazz on 4/18/2015.
  */
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
-	@RequestMapping(name = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index() {
-		return "";
+		return "index";
 	}
 }
